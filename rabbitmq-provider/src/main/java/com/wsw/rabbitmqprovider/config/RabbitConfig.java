34f057fe5f6f0319353925cc1d03fe096137b6de
wangsongwen <2544894086wsw@gmail.com>
2020-08-27 17:21:06 +0800

    8.27-commit

--- rabbitmq-provider/src/main/java/com/wsw/rabbitmqprovider/config/RabbitConfig.java
+++ rabbitmq-provider/src/main/java/com/wsw/rabbitmqprovider/config/RabbitConfig.java
@@ -10,8 +10,7 @@ import org.springframework.context.annotation.Configuration;
 /**
  * @Author WangSongWen
  * @Date: Created in 14:55 2020/7/13
- * @Description: 生产者推送消息的消息确认 回调函数的使用
- *               可以在回调函数根据需求做对应的扩展或者业务数据处理
+ * @Description: 生产者推送消息的消息确认 回调函数的使用。可以在回调函数根据需求做对应的扩展或者业务数据处理
  */
 @Configuration
 public class RabbitConfig {
